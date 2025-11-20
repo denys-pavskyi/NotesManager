7fbc3cc816033a6197e1ffe266cf9c6178d40cba
denys-pavskyi <den.pavski@gmail.com>
2025-11-21 00:17:15 +0200

    feat: Added Note entity and DbContext

---
+++ Backend/NotesManager.DAL/Entities/Note.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NotesManager.DAL.Entities;
+
+public class Note
+{
+    [Key]
+    public Guid Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string Content { get; set; } = null!;
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}
---
+++ Backend/NotesManager.DAL/Persistence/NotesManagesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesManager.DAL.Entities;
+
+namespace NotesManager.DAL.Persistence;
+
+public class NotesManagesDbContext : DbContext
+{
+    public DbSet<Note> Notes { get; set; }
+
+    public NotesManagesDbContext(DbContextOptions<NotesManagesDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
