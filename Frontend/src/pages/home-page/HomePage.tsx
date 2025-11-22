7541e83dc527675a2eb5702f8d2ce3e85f215a4b
denys-pavskyi <den.pavski@gmail.com>
2025-11-22 03:55:45 +0200

    feat: Added empty HomePage and AppRouter

---
+++ Frontend/src/pages/home-page/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.scss';
+
+const HomePage: React.FC = () =>{
+    return (
+        <div className="home-page">
+            <h2>List of notes 123</h2>
+
+        </div>
+    );
+};
+
+export default HomePage;
